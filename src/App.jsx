08d4716653672e136273026a3effb67e12cba6af
Pressturn <Prestonyeo1@gmail.com>
2025-09-19 00:57:28 +0800

    Added favourite and remove favourite feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,46 @@ import { Routes, Route } from "react-router-dom"
 import Header from './components/Header/Header'
 import HomePage from "./pages/HomePage/HomePage"
 import FavouritePage from "./pages/FavouritePage/FavouritePage"
+import { useState } from 'react'
 
 function App() {
+  const [favourites, setFavourites] = useState([])
+
+  function addFavourite(meal) {
+    const fav = {
+      id: meal.idMeal,
+      title: meal.strMeal,
+      thumbnail: meal.strMealThumb,
+    }
+
+  setFavourites(prev =>
+    prev.some(item => item.id === fav.id) ? prev : [...prev, fav]
+  )
+}
+
+function removeFavourite(id) {
+  setFavourites(prev => prev.filter(item => item.id !== id))
+}
 
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path="/favourites" element={<FavouritePage />} />
+        <Route
+          path='/'
+          element={
+            <HomePage onAdd={addFavourite} />
+          }
+        />
+
+        <Route
+          path="/favourites"
+          element={
+            <FavouritePage
+              items={favourites}
+              onDelete={removeFavourite} />
+          }
+        />
       </Routes>
     </>
   )
